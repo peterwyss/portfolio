22b3323b18897e7861da971723681a5403daf6f2
Peter Wyss <peter@aseptica.home>
2020-01-16 07:15:34 +0100

    about formular

--- resources/views/admin/about/about.blade.php
+++ resources/views/admin/about/about.blade.php
@@ -8,15 +8,60 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
+
+                <form  method="POST" action="/admin/about/store" enctype="multipart/form-data">
+                    @csrf    
                 <textarea class="description" name="description"></textarea>
+                <button type="submit" class="btn btn-primary">Submit</button>    
+                </form>
+
 <script src="{{ asset('node_modules/tinymce/tinymce.js') }}"></script>
 <script>
     tinymce.init({
         selector:'textarea.description',
-        width: 900,
+        width: 600,
         height: 300,
-        plugins: 'image',
-        toolbar: 'image'
+        plugins: 'image imagetools',
+        toolbar: 'image ',
+        file_picker_types: 'file image media',
+        file_picker_callback: function (cb, value, meta) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+
+    /*
+      Note: In modern browsers input[type="file"] is functional without
+      even adding it to the DOM, but that might not be the case in some older
+      or quirky browsers like IE, so you might want to add it to the DOM
+      just in case, and visually hide it. And do not forget do remove it
+      once you do not need it anymore.
+    */
+
+    input.onchange = function () {
+      var file = this.files[0];
+
+      var reader = new FileReader();
+      reader.onload = function () {
+        /*
+          Note: Now we need to register the blob in TinyMCEs image blob
+          registry. In the next release this part hopefully won't be
+          necessary, as we are looking to handle it internally.
+        */
+        var id = 'blobid' + (new Date()).getTime();
+        var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+        var base64 = reader.result.split(',')[1];
+        var blobInfo = blobCache.create(id, file, base64);
+        blobCache.add(blobInfo);
+
+        /* call the callback and populate the Title field with the file name */
+        cb(blobInfo.blobUri(), { title: file.name });
+      };
+      reader.readAsDataURL(file);
+    };
+
+    input.click();
+  }
+       
     });
 </script> 
 
